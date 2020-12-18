1544f63125291335c2ab1e4a61193b3266082be3
rwietter <mauriciobw17@gmail.com>
2020-12-18 20:25:30 -0300

    add: initial commit

---
+++ src/pages/blog.js
@@ -0,0 +1,61 @@
+import { graphql, Link } from 'gatsby';
+import React from 'react';
+
+import Bio from '../components/bio';
+import Button from '../components/button';
+import Layout from '../components/layout';
+import SearchPosts from '../components/searchPosts';
+import SEO from '../components/seo';
+
+function Blog({ data, navigate, location }) {
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMdx.edges
+  const localSearchBlog = data.localSearchBlog
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="All posts" />
+      <Bio />
+      <SearchPosts
+        posts={posts}
+        localSearchBlog={localSearchBlog}
+        navigate={navigate}
+        location={location}
+      />
+      <Link to="/">
+        <Button marginTop="85px">Go Home</Button>
+      </Link>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    localSearchBlog {
+      index
+      store
+    }
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Blog
---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+import { Link } from 'gatsby';
+import React from 'react';
+
+import Button from '../components/button';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+function Home({ location }) {
+  const siteTitle = "Gatsby Starter Personal Website"
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="Home" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
+      <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
+      <h1>
+        Hey people{" "}
+        <span role="img" aria-label="wave emoji">
+          👋
+        </span>
+      </h1>
+      <p>Welcome to your new Gatsby website. You are on your home page.</p>
+      <p>
+        This starter comes out of the box with styled components and Gatsby's
+        default starter blog running on Netlify CMS.
+      </p>
+      <p>Now go build something great!</p>
+      <Link to="/blog/">
+        <Button marginTop="35px">Go to Blog</Button>
+      </Link>
+    </Layout>
+  )
+}
+
+export default Home;

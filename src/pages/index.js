7c9815db2a09481c0a16c63268addb810ba9f9ff
rwietter <mauriciobw17@gmail.com>
2020-12-20 11:00:00 -0300

    feat: landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
+import { AnimatePresence, motion, useTransform, useViewportScroll } from 'framer-motion';
 import { graphql, Link, useStaticQuery } from 'gatsby';
-import Img from 'gatsby-image';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -10,6 +10,11 @@ import { GlobalStyle } from '../styles';
 import { rhythm } from '../utils/typography';
 
 function Home({ location }) {
+  const { scrollYProgress } = useViewportScroll()
+
+  const scaleProgressY = useTransform(scrollYProgress, [0.2, 0.4], [0.5, 1])
+  const progressY = useTransform(scrollYProgress, [0.1, 0.2], ["10vh", "0vh"])
+  
   const {
     site: {
       siteMetadata: { title },
@@ -25,7 +30,7 @@ function Home({ location }) {
             title
           }
         }
-        file(relativePath: { eq: "home.png" }) {
+        file(relativePath: { eq: "home2.png" }) {
           childImageSharp {
             fluid(quality: 100) {
               ...GatsbyImageSharpFluid
@@ -37,49 +42,63 @@ function Home({ location }) {
   )
 
   return (
-    <Wrapper>
-      <GlobalStyle />
-      <Container>
-        <Layout location={location} title={title}>
-          <SEO
-            title="Home"
-            keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+    <AnimatePresence>
+      <Wrapper>
+        <GlobalStyle />
+        <Container>
+          <Layout location={location} title={title}>
+            <SEO
+              title="Home"
+              keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+            />
+            <h1>
+              Hey people{" "}
+              <span role="img" aria-label="wave emoji">
+                👋
+              </span>
+            </h1>
+            <p>
+              Welcome to your new Gatsby website. You are on your home page.
+            </p>
+            <p>
+              This starter comes out of the box with styled components and
+              Gatsby's default starter blog running on Netlify CMS.
+            </p>
+            <Link to="/blog/">
+              <Button marginTop="35px">Go to Blog</Button>
+            </Link>
+          </Layout>
+        </Container>
+        <Image />
+        <Background>
+          <Illustration
+            style={{
+              scale: scaleProgressY,
+              y: progressY,
+            }}
           />
-          <h1>
-            Hey people{" "}
-            <span role="img" aria-label="wave emoji">
-              👋
-            </span>
-          </h1>
-          <p>Welcome to your new Gatsby website. You are on your home page.</p>
-          <p>
-            This starter comes out of the box with styled components and
-            Gatsby's default starter blog running on Netlify CMS.
-          </p>
-          <Link to="/blog/">
-            <Button marginTop="35px">Go to Blog</Button>
-          </Link>
-        </Layout>
-      </Container>
-      <Image fluid={fluid} fadeIn alt="background image" />
-      <Background class="background"></Background>
-      <Section></Section>
-      <Footer>
-        © {new Date().getFullYear()}, feito por &nbsp;
-        <a href="https://ufsm.br/" rel="noreferrer" target="_blank">
-          UFSM-FW
-        </a>
-      </Footer>
-    </Wrapper>
+        </Background>
+        <Section></Section>
+        <Footer>
+          <span>
+            © {new Date().getFullYear()}, feito por &nbsp;
+            <a href="https://ufsm.br/" rel="noreferrer" target="_blank">
+              UFSM-FW
+            </a>
+          </span>
+        </Footer>
+      </Wrapper>
+    </AnimatePresence>
   )
 }
 
 const Wrapper = styled.div`
-  position: relative;
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 1fr 0.5fr 0.5fr 0.3fr;
   gap: 10px;
+  width: 100%;
+  height: 100%;
 
   h1 {
     font-size: clamp(1.5rem, 3vw, 2rem);
@@ -105,9 +124,13 @@ const Container = styled.div`
   }
 `
 
-const Image = styled(Img)`
+const Image = styled.img`
+  background-image: url(${require("../data/home.svg")});
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 100%;
   grid-row: 1 / 2;
-  grid-column: 1 / 3;
+  grid-column: 2 / 3;
   @media screen and(min-width: 720px) {
     grid-row: 1 / 2;
     grid-column: 1 / 3;
@@ -118,7 +141,6 @@ const Background = styled.div`
   grid-row: 2 / 3;
   grid-column: 1 / 3;
 
-  display: none;
   width: 100%;
   height: 100%;
   color: #fff;
@@ -126,13 +148,8 @@ const Background = styled.div`
   background-color: #ffffff;
   background-size: cover;
   background-attachment: fixed;
-
-  display: block;
-  background: linear-gradient(
-    50deg,
-    rgba(62, 12, 130, 1) 0.57%,
-    rgba(51, 181, 255, 1) 100%
-  );
+  display: flex;
+  background: linear-gradient(50deg, #ff5555 0.57%, #3e0c82 100%);
 `
 
 const Section = styled.div`
@@ -150,7 +167,7 @@ const Footer = styled.footer`
   width: 99vw;
   max-width: 100vw;
   overflow: hidden;
-  background-image: url(${require('../data/waveGray.svg')});
+  background-image: url(${require("../data/waveGray.svg")});
   background-size: cover;
   height: 100%;
   display: flex;
@@ -159,6 +176,15 @@ const Footer = styled.footer`
   flex-wrap: nowrap;
   font-size: clamp(0.7rem, 3vw, 1rem);
   padding-bottom: ${rhythm(1)};
+`
+
+const Illustration = styled(motion.svg)`
+  background-image: url(${require("../data/Blogging - Colour/blogging.png")});
+  background-repeat: no-repeat;
+  background-size: contain;
+  width: 100%;
+  height: 90%;
+  align-self: center;
 `
 
 export default Home

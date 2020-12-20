e6810d2fefbaeb43bb778081dcbc04a3da8630d8
rwietter <mauriciobw17@gmail.com>
2020-12-20 15:58:43 -0300

    feat: landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,35 @@
-import { AnimatePresence, motion, useTransform, useViewportScroll } from 'framer-motion';
+import { motion, useTransform, useViewportScroll } from 'framer-motion';
 import { graphql, Link, useStaticQuery } from 'gatsby';
-import React from 'react';
-import styled from 'styled-components';
+import React, { useEffect } from 'react';
 
 import Button from '../components/button';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import { GlobalStyle } from '../styles';
-import { rhythm } from '../utils/typography';
+import { events } from './data/data';
+import * as S from './styles';
 
 function Home({ location }) {
   const { scrollYProgress } = useViewportScroll()
 
-  const scaleProgressY = useTransform(scrollYProgress, [0.2, 0.4], [0.5, 1])
-  const progressY = useTransform(scrollYProgress, [0.1, 0.2], ["10vh", "0vh"])
-  
+  const scaleIllustrationProgressY = useTransform(
+    scrollYProgress,
+    [0.2, 0.4],
+    [1, 0.8]
+  )
+
+  const progressIllustrationY = useTransform(
+    scrollYProgress,
+    [0.1, 0.2],
+    ["15vh", "1vh"]
+  )
+
+  const progressPolygonTextX = useTransform(
+    scrollYProgress,
+    [0.1, 0.2],
+    ["10vw", "0vw"]
+  )
+
   const {
     site: {
       siteMetadata: { title },
@@ -41,150 +56,96 @@ function Home({ location }) {
     `
   )
 
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
-    <AnimatePresence>
-      <Wrapper>
-        <GlobalStyle />
-        <Container>
-          <Layout location={location} title={title}>
-            <SEO
-              title="Home"
-              keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-            />
-            <h1>
-              Hey people{" "}
-              <span role="img" aria-label="wave emoji">
-                👋
-              </span>
-            </h1>
-            <p>
-              Welcome to your new Gatsby website. You are on your home page.
-            </p>
-            <p>
-              This starter comes out of the box with styled components and
-              Gatsby's default starter blog running on Netlify CMS.
-            </p>
-            <Link to="/blog/">
-              <Button marginTop="35px">Go to Blog</Button>
-            </Link>
-          </Layout>
-        </Container>
-        <Image />
-        <Background>
-          <Illustration
-            style={{
-              scale: scaleProgressY,
-              y: progressY,
-            }}
+    <S.Wrapper>
+      <GlobalStyle />
+      <S.Container>
+        <Layout location={location} title={title}>
+          <SEO
+            title="Home"
+            keywords={[`blog`, `gatsby`, `javascript`, `react`]}
           />
-        </Background>
-        <Section></Section>
-        <Footer>
-          <span>
-            © {new Date().getFullYear()}, feito por &nbsp;
-            <a href="https://ufsm.br/" rel="noreferrer" target="_blank">
-              UFSM-FW
+          <h1>
+            Hey people{" "}
+            <span role="img" aria-label="wave emoji">
+              👋
+            </span>
+          </h1>
+          <p>Welcome to your new Gatsby website. You are on your home page.</p>
+          <p>
+            This starter comes out of the box with styled components and
+            Gatsby's default starter blog running on Netlify CMS.
+          </p>
+          <Link to="/blog/">
+            <Button marginTop="35px">Go to Blog</Button>
+          </Link>
+        </Layout>
+      </S.Container>
+      <S.Image />
+      <S.Background>
+        <S.Illustration
+          style={{
+            y: progressIllustrationY,
+            scale: scaleIllustrationProgressY,
+          }}
+        />
+        <S.BackgroundText>
+          <motion.h1
+            style={{
+              y: progressPolygonTextX,
+            }}
+          >
+            Lorem, ipsum dolor sit
+          </motion.h1>
+          <motion.p
+            exit="hidden"
+            style={{
+              x: progressPolygonTextX,
+            }}
+          >
+            amet consectetur adipisicing elit. Debitis tempora, eveniet nihil
+            repellendus soluta quasi praesentium. Aperiam quia est quasi, eos
+          </motion.p>
+        </S.BackgroundText>
+      </S.Background>
+      <S.SectionEvents>
+        <h1>Eventos</h1>
+        <S.Events>
+          {events.map(event => (
+            <a href="#">
+              <section key={event.name} className="grid-item">
+                <div className="grid-item_profile">
+                  <img src={event.img} />
+                  <h2>{event.name}</h2>
+                </div>
+                <div className="grid-item__description">
+                  <p>{event.description}</p>
+                  <p>
+                    <span>Data:</span> {event.date}
+                  </p>
+                  <p>
+                    <span>Local:</span> {event.location}
+                  </p>
+                </div>
+              </section>
             </a>
-          </span>
-        </Footer>
-      </Wrapper>
-    </AnimatePresence>
+          ))}
+        </S.Events>
+      </S.SectionEvents>
+      <S.Footer>
+        <span>
+          © {new Date().getFullYear()}, feito para &nbsp;
+          <a href="https://ufsm.br/" rel="noreferrer" target="_blank">
+            UFSM-FW
+          </a>
+        </span>
+      </S.Footer>
+    </S.Wrapper>
   )
 }
-
-const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 0.5fr 0.5fr 0.3fr;
-  gap: 10px;
-  width: 100%;
-  height: 100%;
-
-  h1 {
-    font-size: clamp(1.5rem, 3vw, 2rem);
-  }
-  p {
-    font-size: clamp(0.8rem, 3vw, 1.5rem);
-  }
-`
-
-const Container = styled.div`
-  z-index: 1;
-  grid-row: 1 / 2;
-  grid-column: 1 / 3;
-  height: 100%;
-  justify-self: flex-start;
-  align-self: flex-start;
-  @media screen and(max-width: 720px) {
-    justify-self: flex-end;
-    align-self: flex-start;
-    grid-row: 1 / 2;
-    background-color: red;
-    grid-column: 1 / 2;
-  }
-`
-
-const Image = styled.img`
-  background-image: url(${require("../data/home.svg")});
-  background-size: contain;
-  background-repeat: no-repeat;
-  width: 100%;
-  grid-row: 1 / 2;
-  grid-column: 2 / 3;
-  @media screen and(min-width: 720px) {
-    grid-row: 1 / 2;
-    grid-column: 1 / 3;
-  }
-`
-
-const Background = styled.div`
-  grid-row: 2 / 3;
-  grid-column: 1 / 3;
-
-  width: 100%;
-  height: 100%;
-  color: #fff;
-  clip-path: polygon(0 0, 100% 15%, 100% 89%, 0 100%);
-  background-color: #ffffff;
-  background-size: cover;
-  background-attachment: fixed;
-  display: flex;
-  background: linear-gradient(50deg, #ff5555 0.57%, #3e0c82 100%);
-`
-
-const Section = styled.div`
-  grid-row: 3 / 4;
-  grid-column: 1 / 3;
-  width: 100%;
-  height: 100%;
-  position: relative;
-`
-
-const Footer = styled.footer`
-  display: block;
-  grid-row: 4 / 5;
-  grid-column: 1 / 3;
-  width: 99vw;
-  max-width: 100vw;
-  overflow: hidden;
-  background-image: url(${require("../data/waveGray.svg")});
-  background-size: cover;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-  flex-wrap: nowrap;
-  font-size: clamp(0.7rem, 3vw, 1rem);
-  padding-bottom: ${rhythm(1)};
-`
-
-const Illustration = styled(motion.svg)`
-  background-image: url(${require("../data/Blogging - Colour/blogging.png")});
-  background-repeat: no-repeat;
-  background-size: contain;
-  width: 100%;
-  height: 90%;
-  align-self: center;
-`
 
 export default Home
---
+++ src/pages/styles/index.js
@@ -0,0 +1,211 @@
+import { m as motion } from 'framer-motion';
+import styled from 'styled-components';
+
+import { rhythm } from '../../utils/typography';
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 0.5fr minmax(auto, 1fr) 0.5fr;
+  gap: 10px;
+  width: 100%;
+  height: 100%;
+
+  h1 {
+    font-size: clamp(1.5rem, 3vw, 2rem);
+  }
+  p {
+    font-size: clamp(0.8rem, 3vw, 1.5rem);
+  }
+`
+
+export const Container = styled.div`
+  z-index: 1;
+  grid-row: 1 / 2;
+  grid-column: 1 / 3;
+  height: 100%;
+  justify-self: flex-start;
+  align-self: flex-start;
+  @media screen and(max-width: 720px) {
+    justify-self: flex-end;
+    align-self: flex-start;
+    grid-row: 1 / 2;
+    background-color: red;
+    grid-column: 1 / 2;
+  }
+`
+
+export const Image = styled.svg`
+  background-image: url(${require("../../data/home.svg")});
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 100vh;
+  grid-row: 1 / 2;
+  grid-column: 2 / 3;
+`
+
+export const Background = styled.div`
+  grid-row: 2 / 3;
+  grid-column: 1 / 3;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  place-items: end;
+  place-content: end;
+
+  position: relative;
+
+  max-width: 100%;
+  max-height: 100%;
+  color: #fff;
+  clip-path: polygon(0 0, 100% 15%, 100% 89%, 0 100%);
+  background-color: #ffffff;
+  background-size: cover;
+  background-attachment: fixed;
+  background-image: linear-gradient(to top, #accbee 0%, #e7f0fd 100%);
+
+  @media (min-width: 720px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`
+
+export const BackgroundText = styled.section`
+  display: none;
+  justify-content: center;
+  align-items: flex-start;
+  height: 100%;
+  flex-flow: column;
+  overflow: hidden;
+  h1 {
+    font-size: clamp(0.8rem, 2vw, 1rem);
+  }
+  p {
+    font-size: clamp(0.6rem, 2vw, 0.8rem);
+  }
+  @media (min-width: 720px) {
+    display: flex;
+  }
+`
+
+export const SectionEvents = styled.section`
+  grid-row: 3 / 4;
+  grid-column: 1 / 3;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
+  h1 {
+    font-size: clamp(2rem, 4vw, 2.5rem);
+  }
+`
+
+export const Events = styled.div`
+  grid-row: 3 / 4;
+  grid-column: 1 / 3;
+  width: 100%;
+  height: 100%;
+
+  display: grid;
+  grid-gap: 20px 20px;
+  grid-template-columns: repeat(auto-fill, minmax(14rem, 20rem));
+  grid-template-rows: repeat(auto-fill, minmax(13rem, 1fr));
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  padding: ${rhythm(1)} ${rhythm(1)} 0 ${rhythm(1)};
+
+  .grid-item {
+    width: 100%;
+    height: 100%;
+    background-color: #fefefe;
+    box-shadow: 0px 2px 13px rgba(0, 0, 0, 0.08);
+    border-radius: 4px;
+    padding: 20px;
+    font-family: "Montserrat", "Courier New", Courier, monospace;
+    .grid-item__description {
+      p {
+        font-size: clamp(0.6rem, 2vw, 0.7rem);
+        margin: 0;
+        line-height: 2;
+      }
+      p:first-child {
+        font-size: clamp(0.6rem, 2vw, 0.9rem);
+        font-weight: 600;
+        padding: 0 0 20px 0;
+        line-height: 1.5;
+        @media (min-width: 600px) {
+          line-height: 3;
+        }
+      }
+      p span {
+        font-weight: 600;
+      }
+    }
+  }
+
+  .grid-item_profile {
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    align-items: end;
+    height: 5rem;
+
+    h2 {
+      font-size: clamp(1rem, 2vw, 1.5rem);
+      font-weight: 700;
+      font-family: "Montserrat", "Courier New", Courier, monospace;
+      padding: 0 0 10px 20px;
+    }
+    img {
+      width: 100%;
+      max-width: 3rem;
+      height: 3rem;
+      border-radius: 50%;
+    }
+  }
+
+  @media (min-width: 350px) {
+    grid-template-columns: repeat(auto-fill, minmax(20rem, auto));
+  }
+  @media (min-width: 720px) {
+    grid-template-columns: repeat(auto-fill, minmax(27rem, auto));
+  }
+`
+
+export const Footer = styled.footer`
+  display: block;
+  grid-row: 4 / 5;
+  grid-column: 1 / 3;
+  width: 99vw;
+  max-width: 100vw;
+  overflow: hidden;
+  background-image: url(${require("../../data/waveGray.svg")});
+  background-size: cover;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  flex-wrap: nowrap;
+  font-size: clamp(0.7rem, 3vw, 1rem);
+  padding-bottom: ${rhythm(2)};
+`
+
+export const Illustration = styled(motion.svg)`
+  grid-column: 1 / 3;
+
+  background-image: url(${require("../../data/Blogging - Colour/blogging.svg")});
+  background-repeat: no-repeat;
+  background-size: contain;
+  height: 100%;
+  width: 100%;
+  background-position: 100% 80%;
+
+  @media (min-width: 720px) {
+    background-position: 0 100%;
+    grid-column: 1 / 2;
+  }
+`
